5810e3a37b6eebe3cc26d9061578775077c35535
anuri1 <antipenkovartem1997@gmail.com>
2026-03-17 19:18:09 +0500

    task 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
-class Duck extends Card {
+class Duck extends Creature {
     constructor() {
         super('Мирная утка', 2);
     }
@@ -17,9 +17,15 @@ class Duck extends Card {
     }
 }
 
-class Dog extends Card {
+class Dog extends Creature {
     constructor() {
         super('Пес-бандит', 3);
+    }
+}
+
+class Creature extends Card {
+    getDescriptions() {
+        return [getCreatureDescription(this), super.getDescriptions()];
     }
 }
 
